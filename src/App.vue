10d53afef1107619c13459e578b546dc3251da59
Brooke Smith <brooke.e.smith@tcu.edu>
2020-05-01 13:23:52 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+      </aside>
+      <div class="content">
+      </div>
+    </main>
+  </div>
+  <main>
+  <aside class="sidebar">
+    <router-link
+        v-for="post in posts"
+        active-class="is-active"
+        class="link"
+        :to="{ name: 'post', params: { id: post.id } }">
+      {{post.id}}. {{post.title}}
+    </router-link>
+  </aside>
+  <div class="content">
+    <router-view></router-view>
+  </div>
+</main>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        posts: []
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
+
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      posts: null,
+      endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+    }
+  },
+
+  created() {
+    this.getAllPosts();
+  },
+
+  methods: {
+    getAllPosts() {
+      axios.get(this.endpoint)
+        .then(response => {
+          this.posts = response.data;
+        })
+        .catch(error => {
+          console.log('-----error-------');
+          console.log(error);
+        })
+    }
+  }
+}
+
+<aside class="sidebar">
+  <div v-for="post in posts">
+    {{ post.title }}
+  </div>
+</aside>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './App.vue'
+import Post from './Post.vue'
+Vue.use(Router)
+
+const router = new Router({
+ routes: [
+   {
+     path: '/',
+     name:'home',
+     component: Hello,
+   },
+   {
+     path: '/post/:id',
+     name:'post',
+     component: Post,
+     props: true,
+   },
+ ]
+})
+
+new Vue({
+ el: '#app',
+ render: h => h(App),
+ router
+})
